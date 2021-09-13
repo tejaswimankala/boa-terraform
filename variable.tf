1b48b897279437bcd8bc97399b16c56bd3f67d85
tejaswimankala <79300052+tejaswimankala@users.noreply.github.com>
2021-09-13 13:27:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  default = "skilled-cargo-319212"
+}
+ 
+variable "region" {
+  default = "us-east1"
+}
+ 
+variable "zone" {
+  default = "us-east1-d"
+}
+ 
+variable "cluster" {
+  default = "boa-cluster"
+}
+ 
+variable "token" {
+  default = "ya29.a0ARrdaM-Q3nkmNqI3wjNiaRFWCcG7HyqtxnB5oBB49tFX3f4JZe996LpqUKMWF9BS0Mft8BPhNkU5w2DGJDLdhrncbDSEKfCvaAXbpKoOwtzVqsl9urigW_Ppn7cQbRTA6ZtMi8x9OHZugXWFQYcHPsyMGTX_72ndadqEGlwZAHX108Wbzl49msd-TOfrRs8qbMaElXAurpTS4XCXJcLdEbPy8-qegdjRnSfFc14VTk_LkNGkcfr_hVcyzgAg-HB4dYHmBcRraA"
+}
+ 
+variable "kubernetes_min_ver" {
+  default = "latest"
+}
+ 
+variable "kubernetes_max_ver" {
+  default = "latest"
+}
